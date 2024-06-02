8bf67b66a3abe6282a1673a14d33d685e5dbf9c3
Joshua Hodge <josh@theaudioprogrammer.com>
2024-06-02 09:17:20 -0400

    Cmajor tutorial 04

--- view/index.js
+++ view/index.js
@@ -6,24 +6,77 @@ class MyView extends HTMLElement
        super(); 
        this.patchConnection = patchConnection;
        this.innerHTML = this.getHTML();
+       this.classList.add ("view-patch-element");
     }
 
     connectedCallback()
     {
-        // How we're going to connect Cmajor params w/ HTML elements
+        this.paramListener = event =>
+        {
+            const slider = this.querySelector ("#" + event.endpointID);
+
+            if (slider)
+                slider.value = event.value;
+        };
+
+        this.patchConnection.addAllParameterListener (this.paramListener);
+
+        for (const slider of this.querySelectorAll (".param"))
+        {
+            slider.oninput = () => this.patchConnection.sendEventOrValue (slider.id, slider.value);
+            this.patchConnection.requestParameterValue (slider.id);
+        }
     }
 
     disconnectedCallback()
     {
-        // How we will disconnect when this window is closed
+        this.patchConnection.removeAllParameterListener (this.paramListener);
     }
 
     getHTML()
     {
         return `
-        
-            // write our html and css here
-        
+            <style>
+                .view-patch-element
+                {
+                    font-family: Verdana, sans-serif;
+                    text-align: center;
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+                    padding: 10px;
+                    overflow: auto;
+                    flex-direction: column;
+                    align-items: center;
+                    background-color: lightblue;
+                }
+
+                .view-patch-element #tap-logo
+                {
+                    margin-top: 10px;
+                    top: 10px;
+                    width: 200px;
+                }
+
+                .param
+                {
+                    display: inline-block;
+                    margin: 10px;
+                    width: 200px;
+                }
+
+            </style>
+
+            <body>
+
+                <img id="tap-logo" src="./Resources/tap_logo.png" alt="tap logo">
+
+                <div id="controls">
+                    <input type="range" min="20" max="20000" value="300" class="param" id="frequencyIn">Frequency</input>
+                    <input type="range" min="-84" max="6" value="-12" class="param" id="volume">Gain</input>
+                </div>    
+
+            </body>
         `;   
     }
 }
